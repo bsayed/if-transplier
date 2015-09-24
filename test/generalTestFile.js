736c5a0b4ec349410240b59539799855a992d7a8
Bassam Sayed <bassam@ece.uvic.ca>
2015-09-24 07:17:20 -0700

    Inlining of continue/break and partial return statement support

--- test/generalTestFile.js
+++ test/generalTestFile.js
@@ -1,8 +1,16 @@
 //$old_pc = $pc;
 //while($pc.id !== 'LOOP')
 //    $Λ.pop();
-blah:while(true) {
-    break ;
-}
+//blah:while(true) {
+//    break ;
+//}
+
+foo.x = function (){
+    function bar(){
+        return;
+    }
+    return;
+};
+//obj = {label:""}
 //$Λ[$Λ.len - 2].l;
 //$Λ[$Λ.len − 2] ;
