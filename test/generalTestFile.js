1be59b5ceb4aa85a3dfa479a566dec31b56bd053
Bassam Sayed <bassam@ece.uvic.ca>
2015-09-25 22:36:46 -0700

    Added support for inlining If-Statements.

--- test/generalTestFile.js
+++ test/generalTestFile.js
@@ -4,14 +4,40 @@
 //blah:while(true) {
 //    break ;
 //}
+while(true) {
+    if(true) {
+        x[y] = a;
+        z['w'] = new b[c]();
+        a.b = 'blah';
+        k = 8;
+        break
+    } else {
+        done = true;
+        throw x;
+    }
+}
+
 
 foo.x = function (){
     function bar(){
+        while(true) {
+            if(true) {
+                x[y] = a;
+                z['w'] = new b[c]();
+                a.b = 'blah';
+                k = 8;
+                break
+            } else {
+                done = true;
+                throw x;
+            }
+        }
+
         return 'asdf';
     }
     return;
 };
-foo instanceof Object;
+//foo instanceof Object;
 //obj = {label:""}
 //$Λ[$Λ.len - 2].l;
 //$Λ[$Λ.len − 2] ;
