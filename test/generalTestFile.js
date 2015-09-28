6eded785321339b7fd8b9635ed6aa0c6f90e469b
Bassam Sayed <bassam@ece.uvic.ca>
2015-09-27 18:50:24 -0700

    Added support for FunctionDeclaration and modified the code that push and pop into the scope stack.

--- test/generalTestFile.js
+++ test/generalTestFile.js
@@ -17,34 +17,34 @@
 //    }
 //}
 
-try{
-    //g();
-} catch(x) {
-    h();
-} finally {
-k();
-}
+//try{
+//    //g();
+//} catch(x) {
+//    h();
+//} finally {
+//k();
+//}
 //foo.x = function (){
-//    function bar(){
-//
-//        doWhile:do {
-//            while(x > 0) {
-//                if (true) {
-//                    x[y] = a;
-//                    z['w'] = new b[c]();
-//                    a.b = 'blah';
-//                    k = 8;
-//                    //break
-//                } else {
-//                    done = true;
-//                    throw x;
-//                }
-//            }
-//        }while(y < 0 );
-//
-//        return a;
-//
-//    }
+    function bar(a,b){
+
+        doWhile:do {
+            while(x > 0) {
+                if (true) {
+                    x[y] = a;
+                    z['w'] = new b[c]();
+                    a.b = 'blah';
+                    k = 8;
+                    //break
+                } else {
+                    done = true;
+                    throw x;
+                }
+            }
+        }while(y < 0 );
+
+        return a;
+
+    }
 //    return;
 //};
 //foo instanceof Object;
