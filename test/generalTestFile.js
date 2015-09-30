d8c66686d5f530ed31c86fccc095d3aba0ee4760
Bassam Sayed <bassam@ece.uvic.ca>
2015-09-30 08:02:33 -0700

    Added support for inlining function calls.

--- test/generalTestFile.js
+++ test/generalTestFile.js
@@ -24,32 +24,36 @@
 //} finally {
 //k();
 //}
-//foo.z.y = function (x,y,z){
-//    function bar(a,b){
-//
-//        doWhile:do {
-//            while(x > 0) {
-//                if (true) {
-//                    x[y] = a;
-//                    z['w'] = new b[c]();
-//                    a.b = 'blah';
-//                    k = 8;
-//                    //break
-//                } else {
-//                    done = true;
-//                    throw x;
-//                }
-//            }
-//        }while(y < 0 );
-//
-//        return a;
-//
-//    }
-//    return;
-//};
+//(x instanceof Object)? a=b: x=z;
+x.foo.z = function (x, y, z) {
+    function bar(a, b) {
+
+        //doWhile:do {
+        //    while(x > 0) {
+        //        if (true) {
+        //            x[y] = a;
+        //            z['w'] = new b[c]();
+        //            a.b = 'blah';
+        //            k = 8;
+        //            //break
+        //        } else {
+        //            done = true;
+        //            throw x;
+        //        }
+        //    }
+        //}while(y < 0 );
+
+        return a;
+
+    }
+
+    return bar('x', 'y');
+};
+
+a = x.foo.z(1)
 //foo instanceof Object;
 //var obj;
 //obj.x = {label:"",first:k,second:g()};
-var ar = [x, y,z,w]
+//var ar = [x, y,z,w]
 //$Λ[$Λ.len - 2].l;
 //$Λ[$Λ.len − 2] ;
